276daaf091dd0a094469bc03256f8c76feea6cfa
Subin Siby <mail@subinsb.com>
2021-06-22 15:54:37 +0530

    Add ZWJ test, learning confidence increment test

--- govarnam/govarnam_test.go
+++ govarnam/govarnam_test.go
@@ -53,19 +53,35 @@ func TestGreedyTokenizer(t *testing.T) {
 }
 
 func TestTokenizer(t *testing.T) {
-	expected := []string{"മല", "മാല", "മള", "മലാ", "മളാ", "മാള", "മാലാ", "മാളാ"}
+	// The order of this will fail if VST weights change
+	expected := []string{"മാല", "മല", "മാള", "മള", "മാലാ", "മലാ", "മാളാ", "മളാ"}
 	for i, sug := range varnam.Transliterate("mala").Suggestions {
 		assertEqual(t, sug.Word, expected[i])
 	}
 }
 
 func TestLearn(t *testing.T) {
-	assertEqual(t, varnam.Transliterate("malayalam").Suggestions[0].Word, "മലയലം")
+	assertEqual(t, varnam.Transliterate("malayalam").Suggestions[0].Word, "മാലയലം")
 	varnam.Learn("മലയാളം")
 	assertEqual(t, varnam.Transliterate("malayalam").Suggestions[0].Word, "മലയാളം")
 	assertEqual(t, varnam.Transliterate("malayalaththil").Suggestions[0].Word, "മലയാളത്തിൽ")
 	assertEqual(t, varnam.Transliterate("malayaalar").Suggestions[0].Word, "മലയാളർ")
 	assertEqual(t, varnam.Transliterate("malaykk").Suggestions[0].Word, "മലയ്ക്ക്")
+
+	varnam.Learn("മലയാളത്തിൽ")
+	sugs := varnam.Transliterate("malayala").Suggestions
+
+	assertEqual(t, sugs[0], Suggestion{"മലയാളം", VARNAM_LEARNT_WORD_MIN_CONFIDENCE, sugs[0].LearnedOn})
+	assertEqual(t, sugs[1], Suggestion{"മലയാളത്തിൽ", VARNAM_LEARNT_WORD_MIN_CONFIDENCE, sugs[0].LearnedOn})
+
+	// This word will now be at the top
+	// Test if confidence has increased by one now
+	varnam.Learn("മലയാളത്തിൽ")
+	sug := varnam.Transliterate("malayala").Suggestions[0]
+	assertEqual(t, sug, Suggestion{"മലയാളത്തിൽ", VARNAM_LEARNT_WORD_MIN_CONFIDENCE + 1, sug.LearnedOn})
+
+	// Subsequent pattern can be smaller now (no need of "thth")
+	assertEqual(t, varnam.Transliterate("malayalathil").Suggestions[0].Word, "മലയാളത്തിൽ")
 }
 
 func TestTrain(t *testing.T) {
@@ -87,6 +103,9 @@ func TestZW(t *testing.T) {
 	assertEqual(t, varnam.Transliterate("thaazhvara").Suggestions[0].Word, "താഴ്വര")
 	// _ is ZWNJ
 	assertEqual(t, varnam.Transliterate("thaazh_vara").Suggestions[0].Word, "താഴ്‌വര")
+	// __ is ZWJ
+	assertEqual(t, varnam.Transliterate("n_").Suggestions[0].Word, "ൻ‌") // Old chil
+	assertEqual(t, varnam.Transliterate("nan_ma").Suggestions[0].Word, "നൻ‌മ")
 }
 
 func TestMain(m *testing.M) {
